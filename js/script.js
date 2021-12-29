29df195b7b603e05f41c3ce5c2a3d43c50b67786
AndreyShal <93115383+AndreyShal@users.noreply.github.com>
2021-12-29 17:00:09 +0500

    1

--- js/script.js
+++ js/script.js
@@ -24,18 +24,66 @@ document.getElementById("bags__form-nav").addEventListener('click', () => toggle
 document.getElementById("bags__color-nav").addEventListener('click', () => toggleMe(document.getElementById("bags__color-inside")));
 document.getElementById("bags__material-nav").addEventListener('click', () => toggleMe(document.getElementById("bags__material-inside")));
 
-// let bags__formInsideDSK = document.querySelectorAll(".bags__form-insideDSK li");
-// let bags__colorInsideDSK = document.querySelectorAll(".bags__color-insideDSK li");
-// let bags__materialInsideDSK = document.querySelectorAll(".bags__material-insideDSK li")
-// for (let i = 0; i < bags__formInsideDSK.length; i++) {
-//     document.getElementById("bags__form-navDSK").addEventListener('click', () => toggleMe(bags__formInsideDSK[i]));
-// }
-// for (let i = 0; i < bags__colorInsideDSK.length; i++) {
-//     document.getElementById("bags__color-navDSK").addEventListener('click', () => toggleMe(bags__colorInsideDSK[i]));
-// }
-// for (let i = 0; i < bags__materialInsideDSK.length; i++) {
-//     document.getElementById("bags__material-navDSK").addEventListener('click', () => toggleMe(bags__materialInsideDSK[i]));
-// }
+// ..............................................................MENU__STICKY-START..................................
+// let oldValue = 0;
+// let newValue = 0;
+// window.addEventListener('scroll', (e) => {
+//     newValue = window.pageYOffset;
+//     if (oldValue < newValue) {
+//         // console.log("Up");
+//         document.getElementById("header-desctopID").classList.remove("none__header-mobail");
+//         document.getElementById("navbar").classList.remove("none__header-mobail2");
+//         document.getElementById("navbar2").classList.remove("none__header-mobail3");
+//     }
+//     else if (oldValue > newValue) {
+//         // console.log("Down");
+//         document.getElementById("header-desctopID").classList.add("none__header-mobail");
+//         document.getElementById("navbar").classList.add("none__header-mobail2");
+//         document.getElementById("navbar2").classList.add("none__header-mobail3");
+//     }
+//     oldValue = newValue;
+// });
+
+
+document.addEventListener('DOMContentLoaded', () => { // DOM готов к взаимодейтсвию
+    const onScrollHeader = () => { // объявляем основную функцию onScrollHeader
+        const header = document.querySelector('.header__desctop') // находим header и записываем в константу
+        let prevScroll = window.pageYOffset // узнаем на сколько была прокручена страница ранее
+        let currentScroll // на сколько прокручена страница сейчас (пока нет значения)
+        window.addEventListener('scroll', () => { // при прокрутке страницы
+            currentScroll = window.pageYOffset // узнаем на сколько прокрутили страницу
+            const headerHidden = () => header.classList.contains('header_hidden') // узнаем скрыт header или нет
+            if (currentScroll > prevScroll && !headerHidden()) { // если прокручиваем страницу вниз и header не скрыт
+                header.classList.add('header_hidden') // то скрываем header
+            }
+            if (currentScroll < prevScroll && headerHidden()) { // если прокручиваем страницу вверх и header скрыт
+                header.classList.remove('header_hidden') // то отображаем header
+            }
+            prevScroll = currentScroll // записываем на сколько прокручена страница на данный момент
+        })
+    }
+    onScrollHeader() // вызываем основную функцию onScrollHeader
+});
+document.addEventListener('DOMContentLoaded', () => { // DOM готов к взаимодейтсвию
+    const onScrollHeader1 = () => { // объявляем основную функцию onScrollHeader
+        const header = document.querySelector('.header-mobail') // находим header и записываем в константу
+        let prevScroll = window.pageYOffset // узнаем на сколько была прокручена страница ранее
+        let currentScroll // на сколько прокручена страница сейчас (пока нет значения)
+        window.addEventListener('scroll', () => { // при прокрутке страницы
+            currentScroll = window.pageYOffset // узнаем на сколько прокрутили страницу
+            const headerHidden = () => header.classList.contains('header_hidden1') // узнаем скрыт header или нет
+            if (currentScroll > prevScroll && !headerHidden()) { // если прокручиваем страницу вниз и header не скрыт
+                header.classList.add('header_hidden1') // то скрываем header
+            }
+            if (currentScroll < prevScroll && headerHidden()) { // если прокручиваем страницу вверх и header скрыт
+                header.classList.remove('header_hidden1') // то отображаем header
+            }
+            prevScroll = currentScroll // записываем на сколько прокручена страница на данный момент
+        })
+    }
+    onScrollHeader1() // вызываем основную функцию onScrollHeader
+});
+// ..............................................................MENU__STICKY-END..................................
 /////////////////////////////////////////////////////////////////////////////////////////////////
 /* ..........................................SWIPER СЛАЙДЕР НАЧАЛО........................ */
 var swiper1 = new Swiper('.swiper1', {
